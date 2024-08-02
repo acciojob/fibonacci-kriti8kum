a881842bc1477545f8d94e4fb437ddd75793022f
Kriti Kumari <157258583+kriti8kum@users.noreply.github.com>
2024-08-02 21:54:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function fibonacci(num) {
 // your code here
+	if( num == 0){
+		return 0;
+	} else if(num == 1){
+		return 1;
+	}
+
+	let prev = 0;
+	let curr = 1;
+	for( int i <2; i<=num ; i++){
+		let next = prev + curr ;
+		prev = curr;
+		curr = next;
+	}
+	return prev;
 }
 
 module.exports = fibonacci;
+
